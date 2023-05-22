3c432973eb66433f13e0bf6754f68ae851fe92a8
BaileyMcCauslin <baileymccauslin@gmail.com>
2023-05-22 15:52:33 -0700

    Password reset

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:flutter/gestures.dart';
 
 class Login extends StatelessWidget
 {
@@ -14,6 +15,7 @@ class Login extends StatelessWidget
 
   String email = "";
   String password = "";
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   showAlertDialog( BuildContext context, String statement, User? user ) 
   {
@@ -75,72 +77,120 @@ class Login extends StatelessWidget
     await FirebaseAuth.instance.signOut();
   }
 
+  Future< void > sendResetPasswordEmail( String email, BuildContext context ) async
+  {
+    if( email != "" )
+    {
+      try
+      {
+        await auth.sendPasswordResetEmail( email: email );
+
+        showAlertDialog( context, "Sent password request to the email: ${ email }. If you cannot find it, please check the spam folder or wait a few minutes.", null );
+      }
+      on FirebaseAuthException catch ( error )
+      {
+        String errorMessage = "";
+        switch( error.code )
+        {
+          case "auth/invalid-email":
+            errorMessage = "The email you provided is not valid";
+            break;
+          case "auth/user-not-found":
+            errorMessage = "There is no user with the email provided";
+            break;
+          default:
+            errorMessage = "Something went wrong";
+            break;
+        }
+        //Display pop-up with corresponding error
+        showAlertDialog( context, errorMessage, null );
+      }
+    }
+    else
+    {
+      //Email box left empty
+      showAlertDialog( context, "Please enter your valid email address in the email box.", null );
+    }
+  }
+
 
   void login( String email, String password, BuildContext context ) async
   {
-    try 
+    if( email != "" && password != "" )
     {
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password
-      );
-
-      User? user = credential.user;
-
-      if( user != null )
+      try 
       {
-        if( user.emailVerified )
+        final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+        );
+
+        User? user = credential.user;
+
+        if( user != null )
         {
-          IdTokenResult? userToken = await user?.getIdTokenResult();
-
-          Map<String, dynamic>? claims = userToken?.claims;
-
-          if( claims != null )
+          if( user.emailVerified )
           {
-            if( !claims['admin'] && !claims['manager'] )
+            IdTokenResult? userToken = await user?.getIdTokenResult();
+
+            Map<String, dynamic>? claims = userToken?.claims;
+
+            if( claims != null )
             {
-              showAlertDialog( context, "You don't have the authority to login on this platform", user );
-              
-              //Technically the user is signed in but we dont want this
-              signoutUser();
+              if( !claims['admin'] && !claims['manager'] )
+              {
+                showAlertDialog( context, "You don't have the authority to login on this platform", user );
+                
+                //Technically the user is signed in but we dont want this
+                signoutUser();
+              }
+              else
+              {
+                Navigator.pushNamedAndRemoveUntil( context, '/home', (route) => false ); 
+              }
             }
             else
             {
-              Navigator.pushNamedAndRemoveUntil( context, '/home', (route) => false ); 
+              showAlertDialog( context, "You don't have the authority to login on this platform", user );
+
+              signoutUser();
             }
           }
           else
           {
-            showAlertDialog( context, "You don't have the authority to login on this platform", user );
-            signoutUser();
+            showAlertDialog( context, "You have not verified your email", user );
           }
         }
-        else
-        {
-          showAlertDialog( context, "You have not verified your email", user );
-        }
-      }
-    } 
-    on FirebaseAuthException catch ( error )
-    {
-      switch( error.code )
+      } 
+      on FirebaseAuthException catch ( error )
       {
-        case 'user-not-found':
-          showAlertDialog( context, "No user with that email exists", null );
-          break;
-        case 'wrong-password': 
-          showAlertDialog( context, "Incorrect password for the user with that email", null );
-          break;
-        case 'invalid-email':
-          showAlertDialog( context, "Invalid email.", null );
-          break;
-        case 'user-disabled':
-          showAlertDialog( context, "User disabled", null );
-          break;
-        default:
-          showAlertDialog( context, "Something went wrong. Check that password is correct.", null );
-          break;
+        String errorMessage = "";
+        switch( error.code )
+        {
+          case 'user-not-found':
+            errorMessage = "No user with that email exists";
+            break;
+          case 'wrong-password':
+            errorMessage = "Incorrect password for the user with that email";
+            break;
+          case 'invalid-email':
+            errorMessage = "Invalid email";
+            break;
+          case 'user-disabled':
+            errorMessage = "User disabled";
+            break;
+          default:
+            errorMessage = "Something went wrong. Check that password is correct.";
+            break;
+        }
+
+        //Display pop-up with corresponding error message
+        showAlertDialog( context, errorMessage, null );
       }
+    }
+    else
+    {
+      showAlertDialog( context, "One of the mandatory fields is empty.", null );
     }
   }
 
@@ -222,6 +272,22 @@ class Login extends StatelessWidget
                             },
                             child: Text('Login'),
                     )
+                ),
+                new Container(
+                    height: windowSize.maxHeight / 10,
+                    width: windowSize.maxWidth / 5,
+                    padding: const EdgeInsets.symmetric( vertical: 20 ),
+                    margin: EdgeInsets.only( top: 350, right: windowSize.maxWidth / 2.5, left: windowSize.maxWidth / 2.5 ),
+                    child:
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(color: Colors.blue),
+                          text: "Forgot Password",
+                          recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            sendResetPasswordEmail( email, context );
+                          }),
+                        ),
                 ),
             ],
         ),
