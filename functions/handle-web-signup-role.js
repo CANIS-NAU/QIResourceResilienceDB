53766f32f5ecc18c5781e9f55b4339069bd81ae1
BaileyMcCauslin <baileymccauslin@gmail.com>
2024-01-16 08:50:53 -0700

    Added comments and ready to test

--- functions/handle-web-signup-role.js
+++ functions/handle-web-signup-role.js
@@ -3,6 +3,9 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 
+// TODO: Test locally: https://firebase.google.com/docs/functions/local-emulator
+//       and possibly use postman
+
 const adminApp = admin.initializeApp(functions.config().firebase);
 
 const app = express();
@@ -16,9 +19,10 @@ app.use(express.json());
 const SUCCESSFUL_RES = 200;
 const UNSUCCESSFUL_RES = 500;
 
+// TODO: Investigate what res.status.send returns to caller here, may need to
+//       return the error object itself to show error in func. 
 function getCustomClaims(res, token) {
   admin.auth(adminApp).verifyIdToken(token).then((decodedToken) => {
-    // Get user that made the req
     admin.auth(adminApp).getUser(decodedToken.uid).then((userRecord) => {
       return userRecord.customClaims;
     }).catch((error) => {
@@ -29,16 +33,24 @@ function getCustomClaims(res, token) {
   });
 }
 
+// Set response attributes
+// res -> Response to send back to client
+// origins -> Allowed origins of response
+// methods -> The method of the response: POST, GET, ect..
+// headers -> The allowed headers of the response
+function setResAttr(res,origins,methods,headers) {
+  res.set('Access-Control-Allow-Origin',origins);
+  res.set('Access-Control-Allow-Methods',methods);
+  res.set('Access-Control-Allow-Headers',headers);
+  return res;
+}
+
 app.post('/handleWebSignUpRole', async (req, res) => {
 
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'POST');
-  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res = setResAttr(res,'*','POST','Content-Type, Authorization');
 
   const MANAGER_STR = "manager";
   const ADMIN_STR = "admin";
-
-  //Global state varibles
   const reqInfo = {
     'body': req.body,
     'email': req.body.email,
@@ -51,7 +63,7 @@ app.post('/handleWebSignUpRole', async (req, res) => {
     let managerBool = reqInfo['role'] === MANAGER_STR;
     let adminBool = reqInfo['role'] === ADMIN_STR;
 
-    if(adminBool || managerBool){
+    if(adminBool || managerBool) {
       try {
         // Create a new user with provided creds
         const userRecord = await admin.auth().createUser({
@@ -63,16 +75,17 @@ app.post('/handleWebSignUpRole', async (req, res) => {
         await admin.auth().setCustomUserClaims(uid, {
           MANAGER_STR: managerBool,
           ADMIN_STR: adminBool,
+          'disabled': false,
         });
 
         // Nothing went wrong, return success
         return res.status(SUCCESSFUL_RES).send({'data': userRecord});
       } 
-      catch(error){
+      catch(error) {
         return res.status(UNSUCCESSFUL_RES).send({'error': error});
       }
     }
-    else{
+    else {
       return res.status(UNSUCCESSFUL_RES).send({'error': "Not a correct role"});
     }
   }
@@ -86,15 +99,10 @@ app.post('/handleWebSignUpRole', async (req, res) => {
   }
 });
 
-// TODO: Test using local emulation
 app.post('/getUsers', async (req,res) => {
 
-  // TODO: Maybe split to a func
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'POST');
-
-  // Diff content type?
-  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // TODO: Potentially change params here
+  res = setResAttr(res,'*','POST','Content-Type, Authorization');
 
   const reqInfo = {
     'body': req.body,
@@ -110,10 +118,10 @@ app.post('/getUsers', async (req,res) => {
           userList.append(userRecord);
         });
 
-        if (listUsersResult.pageToken) {
+        if(listUsersResult.pageToken) {
           listAllUsers(listUsersResult.pageToken);
         }
-        else{
+        else {
           return userList;
         }
       }).catch((error) => {
@@ -130,9 +138,39 @@ app.post('/getUsers', async (req,res) => {
   }
 });
 
-// TODO: Disable user with custom claims
+// TODO: Check roles on client side
+app.post('/updateAccountStatus', async (req,res) => { 
+  
+  // TODO: Potentially change params here
+  res = setResAttr(res,'*','POST','Content-Type, Authorization');
 
+  // Get info from req
+  const reqInfo = {
+    'body': req.body,
+    'uid': req.body.uid,
+    'token': req.body.token
+  };
 
-// exports.disableUsers = functions.https.onRequest(app);
+  // Get users claims 
+  const claims = getCustomClaims(res,reqInfo['token']);
+
+  // Check for valid claims
+  if(claims !== null) {
+    // TODO: May need to wrap in async arrow func
+    await admin.auth().setCustomUserClaims(reqInfo['uid'], {
+      // TODO: On already existing accounts, diabled field DNE
+      'disabled': !claims['disabled']
+    });
+    
+    return res.status(SUCCESSFUL_RES).send(
+                                         {'Success': "changed account status"});
+  }
+  else {
+    return res.status(UNSUCCESSFUL_RES).send(
+                                  {'error': "could not change account status"});
+  }
+});
+
 exports.handleWebSignUpRole = functions.https.onRequest(app);
-exports.getUsers = functions.https.onRequest(app);
+exports.getUsers            = functions.https.onRequest(app);
+exports.updateAccountStatus = functions.https.onRequest(app);
