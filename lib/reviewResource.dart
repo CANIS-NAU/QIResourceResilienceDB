80c65336e16c3226f966ee1d0872e9725325a122
Shelby Hagemann <52891544+shelbyhagemann@users.noreply.github.com>
2024-02-05 14:51:50 -0700

    Fixed life experiences and accessibility rating

--- lib/reviewResource.dart
+++ lib/reviewResource.dart
@@ -181,7 +181,7 @@ class _ReviewResourceState extends State<ReviewResource> {
     'Specific to people living away from home (e.g., college students or people living away from Hopi)',
   ];
 
-  final List<bool> selectedExperiences = List<bool>.filled(6, false);
+  final List<bool> selectedExperiences = List<bool>.filled(5, false);
   List<String> selectedLifeExperiences = [];
 
   //Potential accessibility features
@@ -193,7 +193,7 @@ class _ReviewResourceState extends State<ReviewResource> {
     'Resource is related to a sober living facility, which has been verified by ADHS and AHCCCS.',
   ];
 
-  final List<bool> selectedAccessibility = List<bool>.filled(6, false);
+  final List<bool> selectedAccessibility = List<bool>.filled(5, false);
   List<String> selectedAccessibilityFeatures = [];
 
   // take in the name of the standard and description and displays it
@@ -907,10 +907,10 @@ class _ReviewResourceState extends State<ReviewResource> {
                                                   selectedExperiences[index] =
                                                       value!;
                                                   if (value) {
-                                                    lifeExperiences.add(
+                                                    selectedLifeExperiences.add(
                                                         lifeExperiences[index]);
                                                   } else {
-                                                    lifeExperiences.remove(
+                                                    selectedLifeExperiences.remove(
                                                         lifeExperiences[index]);
                                                   }
                                                 });
@@ -959,10 +959,10 @@ class _ReviewResourceState extends State<ReviewResource> {
                                             selectedAccessibility[index] =
                                                 value!;
                                             if (value) {
-                                              accessibilityFeatures.add(
+                                              selectedAccessibilityFeatures.add(
                                                   accessibilityFeatures[index]);
                                             } else {
-                                              accessibilityFeatures.remove(
+                                              selectedAccessibilityFeatures.remove(
                                                   accessibilityFeatures[index]);
                                             }
                                           });
