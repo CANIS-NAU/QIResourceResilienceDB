52b2d59f99f4675cc4e798aa84b5bb65292d0cdb
nicolesylvester <niccisylvester@gmail.com>
2025-03-20 13:00:26 -0700

    code clean up

--- lib/analytics_provider.dart
+++ lib/analytics_provider.dart
@@ -4,20 +4,9 @@ import 'package:web_app/Analytics.dart';
 class AnalyticsProvider extends ChangeNotifier {
   final HomeAnalytics _analytics = HomeAnalytics();
 
-  // flag controlling whether or not to track link clicks
-  bool _shouldTrackLinks = true;
-
   HomeAnalytics get analytics => _analytics;
 
-  bool get shouldTrackLinks => _shouldTrackLinks;
-  set shouldTrackLinks(bool value) {
-    _shouldTrackLinks = value;
-    notifyListeners(); 
-  }
-
   void submitClickedLink(String type, Uri link, String resourceId) {
-    if (_shouldTrackLinks) {
       _analytics.submitClickedLink(type, link, resourceId);
-    }
   }
 }
