9a208257f5a85e78da45c9cb9833289e4076c9eb
Tyler <tyler.bryant117@gmail.com>
2025-05-28 12:01:16 -0700

    Added cost label map

--- lib/model.dart
+++ lib/model.dart
@@ -244,4 +244,17 @@ class Resource {
     'good': 'Good Cultural Responsiveness',
     'high': 'Specific resource for Hopi community'
   };
+
+  static const Map<String, String> costLabels = {
+    'free': 'Free',
+    'insurance_covered': 'Covered by insurance',
+    'insurance_copay': 'Covered by insurance with copay',
+    'income_scale': 'Sliding scale (income-based)',
+    'donation': 'Pay what you can/donation-based',
+    'payment_plan': 'Payment plans available',
+    'subscription': 'Subscription',
+    'fee': 'One-time fee',
+    'free_trial': 'Free trial period'
+  }
+
 }
